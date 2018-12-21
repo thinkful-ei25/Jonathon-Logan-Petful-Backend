5b16fc0ad65f619f5e94064eaad360260b8ca6d1
Logan Wang <loganxwang@hotmail.com>
2018-12-21 07:52:37 -0800

    added dog/cat test endpoints

--- index.js
+++ index.js
@@ -22,6 +22,37 @@ app.use(
   })
 );
 
+//dog and cat endpoints
+app.get('/api/cat', (req, res, next) => {
+  let cat = [
+    {
+      imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+      imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+      name: 'Fluffy',
+      sex: 'Female',
+      age: 2,
+      breed: 'Bengal',
+      story: 'Thrown on the street'
+    }
+  ];
+
+  res.json(cat);
+});
+app.get('/api/dog', (req, res, next) => {
+  let dog = [
+    {
+      imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+      imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+      name: 'Zeus',
+      sex: 'Male',
+      age: 3,
+      breed: 'Golden Retriever',
+      story: 'Owner Passed away'
+    }
+  ];
+  res.json(dog);
+});
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
